601b8b982a4070d47cc0c57ab642d78e3b50a33c
luis-fellipe-pepa <149219589+luis-fellipe-pepa@users.noreply.github.com>
2023-12-20 00:52:19 -0300

    Update destinatario.php

--- destinatario.php
+++ destinatario.php
@@ -1,15 +1,15 @@
 <?php
-$nome = addcslashes($_POST['nome']);
-$email = addcslashes($_POST['email']);
-$celular = addcslashes($_POST['celular']);
-$mensagem = addcslashes($_POST['mensagem']);
-$para = "luisfellipe421@gmail.com";
-$assunto = "coleta - portifolio";
-$corpo = "Nome:" . $nome . "\n" . "E-mail: " . $email . "\n" . "Telefone: " . $celular . "\n" . "Mensagem: " . $mensagem . "\n";
-$cabeca = "From: luisfellipediasgarcia@gmail.com" . "\n" . "Reply-To: " . $email . "\n" . "X-Mailer: PHP/" . phpversion();
-if (mail($para, $assunto, $corpo, $cabeca)) {
-    echo "E-mail enviado com sucesso";
-} else {
-    echo "Houve um erro";
+$nome=addcslashes($_POST['nome']);
+$email =addcslashes($_POST['email']);
+$celular=addcslashes($_POST['celular']);
+$mensagem=addcslashes($_POST['mensagem']);
+$para="luisfellipe421@gmail.com"
+$assunto="coleta - portifolio"
+$corpo="Nome:".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$celula."\n"."Mensagem: ".$mensagem."\n";
+$cabeca="from:luisfellipediasgarcia@gmail.com"."\n"."reply-to: ". $email."\n"."X=Mailer:PHP/".phpversion();
+if(mail($para,$assunto,$corpo,$cabeca)){
+    echo("e-mail enviado com sucesso")
+}else{
+    echo("houve um erro")
 }
 ?>
